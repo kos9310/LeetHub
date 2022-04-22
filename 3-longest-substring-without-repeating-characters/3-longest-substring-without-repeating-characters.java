f6f6b5d4ff685c28b9f61e5cade8865f0e812c0f
kos9310 <50097764+kos9310@users.noreply.github.com>
2022-04-22 16:53:12 +0900

    Time: 14 ms (38.75%), Space: 44.5 MB (57.98%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -5,9 +5,10 @@ class Solution {
         for(int i=0; i<len; i++) {
             char c = s.charAt(i);
             if(map.containsKey(c)) {
-                if(map.get(c) > start) {
-                    cnt = i - map.get(c) - 1;
-                    start = map.get(c);
+                int val = map.get(c);
+                if(val > start) {
+                    cnt = i - val - 1;
+                    start = val;
                 }
             }
             cnt++;
