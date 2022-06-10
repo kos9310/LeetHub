dad3a6126bf223cffa365cc4181cd4c829be8d05
kos9310 <50097764+kos9310@users.noreply.github.com>
2022-06-10 16:54:44 +0900

    Time: 1241 ms (28.62%), Space: 0B (100.00%) - LeetHub

--- 180-consecutive-numbers/180-consecutive-numbers.sql
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -1,14 +1,5 @@
-with aa as (
-select *, row_number() over(order by id) rn
-from logs a
-where num = (select num from logs where id = a.id-1)
-), bb as
-(
-SELECT id, num, id-rn as diff
-from aa
-)
+
 select distinct num consecutivenums
-from bb
-WHERE diff in (select diff from bb
-group by diff
-having count(diff) >= 2)
+from logs a
+where num = (select num from logs where a.id = id+1)
+and num = (select num from logs where a.id = id+2)
